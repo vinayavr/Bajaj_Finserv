f40f2325bb4f35b3ec3d25cc88cfbe567b1a14e7
vinayavr <10337vinaya.v.r@gmail.com>
2026-04-24 20:19:40 +0530

    Fix for api deployment

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "frontend")));
+
 
 function checkIsValidEdge(raw) {
   if (typeof raw !== "string")
