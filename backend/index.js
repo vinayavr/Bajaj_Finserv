c889d8f2c7f6f1c6d33138d78391e6a9d0850a2d
vinayavr <10337vinaya.v.r@gmail.com>
2026-04-24 19:52:11 +0530

    checkin

--- backend/index.js
+++ backend/index.js
@@ -248,6 +248,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
